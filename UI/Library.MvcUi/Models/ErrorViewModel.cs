68ad6686bc3504f7eda4a4b08092a4014e40e15d
Guven Boydak <gvn.boydak@gmail.com>
2023-06-20 13:28:38 +0300

    Add project files.

---
+++ UI/Library.MvcUi/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Library.MvcUi.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
